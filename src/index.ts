d42b2fa61e3cdd0db169715a45809da9b48ae876
grantsru <grantsru@gmail.com>
2020-07-29 15:40:43 -0400

    Short summary on Express, use ts-jest for testing

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,47 @@
-import express from "express";
+import express from 'express';
 
 const app = express();
-const port=1234;
+const port = 1234;
 
-app.get( '/', (req, res) => {
-    res.send("Hello!");
+const authCheck = (req, res, next) => {
+  if (req.query.bar === '1') next();
+  else res.status(401).json('You shall not pass!');
+};
+
+const loggerMiddleware = (req, res, next) => {
+  console.log('Yo, just got a new request!');
+  next();
+};
+
+const badMiddleware = (req, res, next) => {
+  console.log('I AM SO BAD');
+  next();
+};
+
+app.use(loggerMiddleware);
+app.use(badMiddleware);
+app.use(authCheck);
+
+app.get('/', (req, res) => {
+  res.send('Hello!');
 });
 
+app.post('/', (req, res) => {
+  const databaseResponse = {
+    id: 1,
+    imageUrl: '/beautiful-sunrise.jpg',
+    friendTags: [123, 234, 345],
+  };
 
-app.listen( port, () => {
-    console.log('server started at port ${ port }');
+  res.json(databaseResponse);
+});
+
+const getFoo = (req, res) => {
+  res.send(`Yo, did you send me this? ${req.query.bar}`);
+};
+
+app.get('/foo', getFoo);
+
+app.listen(port, () => {
+  console.log(`server started at port ${port}`);
 });
